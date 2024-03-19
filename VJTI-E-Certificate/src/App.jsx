b74229edbb9e5e0d25c41089169e1396ca82a442
Aditya Arakharao <ararakharao_b21@it.vjti.ac.in>
2024-03-19 14:31:02 +0530

    &committee

--- VJTI-E-Certificate/src/App.jsx
+++ VJTI-E-Certificate/src/App.jsx
@@ -1,21 +1,23 @@
-import './App.css'
-import Form from './components/Form';
-import {Banner} from './components/Home'
+import "./App.css";
+import Form from "./components/Form";
+import { Banner } from "./components/Home";
 import "bootstrap/dist/css/bootstrap.min.css";
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import { NavBar } from './components/NavBar';
-import Login from './components/Login';
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { NavBar } from "./components/NavBar";
+import Login from "./components/Login";
+import { Skills } from "./components/Skills";
+import { Projects } from "./components/Projects";
 function App() {
   return (
     <>
-         
-        <NavBar/>
-        <Banner/>
-        {/* <Form/> */}
-        {/* <Login/> */}
-         
+      <NavBar />
+      <Banner />
+      <Skills />
+      <Projects />
+      {/* <Form/> */}
+      {/* <Login/> */}
     </>
-  )
+  );
 }
 
 export default App;
