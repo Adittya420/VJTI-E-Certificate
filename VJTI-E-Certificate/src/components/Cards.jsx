11a4a0df6a0c23bf9edcc821664a8c638583ac69
rushirajput18 <rushikeshrajput666@gmail.com>
2024-03-20 16:25:42 +0530

    Cards Added

--- VJTI-E-Certificate/src/components/Cards.jsx
+++ VJTI-E-Certificate/src/components/Cards.jsx
@@ -1,22 +1,17 @@
 import React from "react";
-import "../css/Cards.css";
 import { Link } from "react-router-dom";
-function Cards(props) {
-  return (
-    <div class="card">
-    <p class="card-title"></p>
-    <p class="card-des">
-      <img src={props.img} alt="" srcset="" />
-      <h3>{props.name}</h3>
-    </p>
-    <p class="card-text">
-      <Link style={{color:"white"}} to="/form">View More</Link>
-      <svg class="arrow-icon" stroke="currentColor" fill="currentColor" stroke-width="0" viewBox="0 0 16 16" height="1em" width="1em" xmlns="http://www.w3.org/2000/svg">
-        <path fill-rule="evenodd" d="M4 8a.5.5 0 0 1 .5-.5h5.793L8.146 5.354a.5.5 0 1 1 .708-.708l3 3a.5.5 0 0 1 0 .708l-3 3a.5.5 0 0 1-.708-.708L10.293 8.5H4.5A.5.5 0 0 1 4 8z"></path>
-      </svg>
-    </p>
-  </div>
-  );
-}
+import "../css/Cards.css";
 
-export default Cards;
+export const Cards = ({ title, description, imgUrl }) => {
+  return (
+    <div className="card" style={{paddingLeft:'30px'}}>
+      <div className="card-title" style={{paddingLeft:'15px'}}>
+        <h3>{title}</h3>
+      </div>
+      <div className="card-des">
+        <img src={imgUrl} alt="" style={{width:'200px'}}/>
+      </div>
+    
+    </div>
+  );
+};
