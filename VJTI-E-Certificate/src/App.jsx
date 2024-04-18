dde88c49c0496dfaf390f30c89319f81f10b4c53
rushirajput18 <rushikeshrajput666@gmail.com>
2024-04-18 12:34:17 +0530

    Admin Pages

--- VJTI-E-Certificate/src/App.jsx
+++ VJTI-E-Certificate/src/App.jsx
@@ -1,40 +1,49 @@
 import "./App.css";
 import { Banner } from "./components/Home";
 import "bootstrap/dist/css/bootstrap.min.css";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes,
+  Outlet,
+} from "react-router-dom";
 import { NavBar } from "./components/NavBar";
 import Authentication from "./components/Login";
 import { Skills } from "./components/Skills";
 import { Projects } from "./components/Projects";
-import Register from "./components/Register";
+import Register from "./components/Regsiter";
 
 import Form from "./components/Form";
 import AddCommittee from "./components/AddCommittee";
 import { USER } from "./components/Global";
 import { Footer } from "./components/Footer";
+import AddCertificate from "./components/AddCertificate";
 
 function App() {
   return (
     <>
       <Router>
-        {USER === "user" && <NavBar />}
+        {USER === "admin" && <NavBar />}
         <Routes>
           <Route
             path="/"
             element={
               <>
-                {USER === "user" && <Banner />}
-                <Projects />
+                {USER === "admin" && <Banner />}
+                {USER === "admin" && <Projects />}
+                <Outlet />
                 {/* <Footer /> */}
               </>
             }
           />
 
           <Route exact path="/form" element={<Form />} />
-          <Route exact path="/addCertificate" element={<Form />} />
+          <Route exact path="/addCertificate" element={<AddCertificate />} />
           {/* Add a route for the Form component */}
 
           <Route exact path="/login" element={<Authentication />} />
+          {/* <Route path="/skills" element={<Skills />} /> */}
+          <Route path="/projects" element={<Projects />} />
         </Routes>
       </Router>
     </>
--- VJTI-E-Certificate/src/components/Global.jsx
+++ VJTI-E-Certificate/src/components/Global.jsx
@@ -1,3 +1,4 @@
 // constants.js
 export const API_BASE_URL = "https://api.example.com";
-export const USER = "user";
+export const USER = "admin";
+export const isLoggedIn = "false";
